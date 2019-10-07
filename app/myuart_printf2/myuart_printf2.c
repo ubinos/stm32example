4fcfd4484a92f864d1879fd3884654dac8ef0e21
Sungho Park <siminsungho@gmail.com>
2019-10-07 15:03:03 +0900

    Add ubinos config "myuart_printf2_stm3221geval"

---
+++ app/myuart_printf2/myuart_printf2.c
@@ -0,0 +1,217 @@
+#include <ubinos.h>
+
+#if (INCLUDE__APP__myuart_printf2 == 1)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "main.h"
+
+UART_HandleTypeDef UartHandle;
+
+#ifdef __GNUC__
+/* With GCC Compilers, small printf (option LD Linker->Libraries->Small printf set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+	#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+static void Error_Handler(void);
+static void task1func(void *arg);
+static void task2func(void *arg);
+
+int appmain(int argc, char *argv[]) {
+	int r;
+
+	HAL_Init();
+
+	UartHandle.Instance = USARTx;
+
+	UartHandle.Init.BaudRate = 115200;
+	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+	UartHandle.Init.StopBits = UART_STOPBITS_1;
+	UartHandle.Init.Parity = UART_PARITY_NONE;
+	UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	UartHandle.Init.Mode = UART_MODE_TX_RX;
+	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+
+	if (HAL_UART_Init(&UartHandle) != HAL_OK) {
+		Error_Handler();
+	}
+
+	printf("\n\r My UART Printf 2 Example: retarget the C library printf function to the UART\n\r");
+
+	//
+	printf("\n\n\r\n");
+	printf("================================================================================\r\n");
+	printf("myrtc2 (build time: %s %s)\r\n", __TIME__, __DATE__);
+	printf("================================================================================\r\n");
+	printf("\r\n");
+#if (UBINOS__UBICLIB__USE_MALLOC_RETARGETING == 1)
+	r = heap_printheapinfo(NULL);
+	if (0 == r) {
+		printf("\r\n");
+		printf("================================================================================\r\n");
+		printf("\r\n");
+	}
+#endif /* (UBINOS__UBICLIB__USE_MALLOC_RETARGETING == 1) */
+
+	srand(time(NULL));
+
+	r = task_create(NULL, task1func, NULL, task_getmiddlepriority(), 0, "task1");
+	if (0 != r) {
+		logme("fail at task_create\r\n");
+	}
+
+	r = task_create(NULL, task2func, NULL, task_getmiddlepriority(), 0, "task2");
+	if (0 != r) {
+		logme("fail at task_create\r\n");
+	}
+
+	ubik_comp_start();
+
+	return 0;
+}
+
+static void task1func(void *arg) {
+	unsigned int delayms;
+
+	task_sleepms(1000);
+
+	for (unsigned int i = 0;; i++) {
+		delayms = (rand() % 10 + 1) * 200;
+		printf("1: hello world ! (%u) (delay = %4d ms)\r\n", i, delayms);
+		task_sleepms(delayms);
+	}
+}
+
+static void task2func(void *arg) {
+	unsigned int delayms;
+
+	task_sleepms(1000);
+
+	for (unsigned int i = 0;; i++) {
+		delayms = (rand() % 10 + 1) * 200;
+		printf("2: hello world ! (%u) (delay = %4d ms)\r\n", i, delayms);
+		task_sleepms(delayms);
+	}
+}
+PUTCHAR_PROTOTYPE {
+	/* Place your implementation of fputc here */
+	/* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+	HAL_UART_Transmit(&UartHandle, (uint8_t*) &ch, 1, 0xFFFF);
+
+	return ch;
+}
+
+static void Error_Handler(void) {
+	/* Turn LED3 on */
+	BSP_LED_On(LED3);
+	while (1) {
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar(void) __attribute__((weak));
+
+__attribute__((weak)) int _read(int file, char *ptr, int len) {
+	int DataIdx;
+
+	for (DataIdx = 0; DataIdx < len; DataIdx++) {
+		*ptr++ = __io_getchar();
+	}
+
+	return len;
+}
+
+__attribute__((weak)) int _write(int file, char *ptr, int len) {
+	int DataIdx;
+
+	for (DataIdx = 0; DataIdx < len; DataIdx++) {
+		__io_putchar(*ptr++);
+	}
+	return len;
+}
+
+/**
+ * @brief UART MSP Initialization
+ *        This function configures the hardware resources used in this example:
+ *           - Peripheral's clock enable
+ *           - Peripheral's GPIO Configuration
+ * @param huart: UART handle pointer
+ * @retval None
+ */
+void HAL_UART_MspInit(UART_HandleTypeDef *huart) {
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	/*##-1- Enable peripherals and GPIO Clocks #################################*/
+	/* Enable GPIO TX/RX clock */
+	USARTx_TX_GPIO_CLK_ENABLE();
+	USARTx_RX_GPIO_CLK_ENABLE();
+
+	/* Enable USARTx clock */
+	USARTx_CLK_ENABLE();
+
+	/*##-2- Configure peripheral GPIO ##########################################*/
+	/* UART TX GPIO pin configuration  */
+	GPIO_InitStruct.Pin = USARTx_TX_PIN;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+	GPIO_InitStruct.Alternate = USARTx_TX_AF;
+
+	HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
+
+	/* UART RX GPIO pin configuration  */
+	GPIO_InitStruct.Pin = USARTx_RX_PIN;
+	GPIO_InitStruct.Alternate = USARTx_RX_AF;
+
+	HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
+}
+
+/**
+ * @brief UART MSP De-Initialization
+ *        This function frees the hardware resources used in this example:
+ *          - Disable the Peripheral's clock
+ *          - Revert GPIO and NVIC configuration to their default state
+ * @param huart: UART handle pointer
+ * @retval None
+ */
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart) {
+	/*##-1- Reset peripherals ##################################################*/
+	USARTx_FORCE_RESET();
+	USARTx_RELEASE_RESET();
+
+	/*##-2- Disable peripherals and GPIO Clocks #################################*/
+	/* Configure UART Tx as alternate function  */
+	HAL_GPIO_DeInit(USARTx_TX_GPIO_PORT, USARTx_TX_PIN);
+	/* Configure UART Rx as alternate function  */
+	HAL_GPIO_DeInit(USARTx_RX_GPIO_PORT, USARTx_RX_PIN);
+
+}
+
+#endif /* (INCLUDE__APP__myuart_printf2 == 1) */
+
