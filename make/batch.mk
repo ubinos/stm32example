057b5ffac3b3b09ead802071a5217f3b0ea99077
Sungho Park <siminsungho@gmail.com>
2019-10-09 20:43:16 +0900

    Remove ubinos config "myuart_printf_stm3221geval_baremetal", "mygpio_exti_stm3221geval_baremetal"

--- make/batch.mk
+++ make/batch.mk
@@ -8,19 +8,7 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=myuart_printf_stm3221geval_baremetal
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
 	make -f makefile.mk $@ CONFIG_NAME=myuart_printf2_stm3221geval
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=mygpio_exti_stm3221geval_baremetal
 	@echo ""
 	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
